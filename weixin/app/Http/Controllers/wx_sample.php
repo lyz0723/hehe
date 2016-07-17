ef7386e0bef108de3642a226a597e5629082ed58
lyz <360195045@qq.com>
2016-07-17 13:39:46 +0800

    text

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -23,24 +23,20 @@ class wechatCallbackapiTest
 
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                //发送方账号(一个openid)
-                $fromUsername = $postObj->FromUserName;
-                //开发者微信号
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-//                //获取用户发送消息的类型
-                $msgType=$postObj->MsgType;
-                $time = time();
-                $textTpl = "<xml>
+        //extract post data
+        if (!empty($postStr)){
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
@@ -48,26 +44,20 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";
-            if($msgType=="text"){
-                if(!empty( $keyword ))
-                {
-                    if($keyword="文本"){
-                        $msgType = "text";
-                        $contentStr = "Welcome to wechat world!";
-                        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                        echo $resultStr;
-                    }
-                }else{
-                    echo "Input something...";
-                }
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
             }
+
         }else {
             echo "";
             exit;
         }
-
-
-
     }
 		
 	private function checkSignature()
