54ae85a033ad27cd3fe3caf858c6dbc59b078f53
lyz <360195045@qq.com>
2016-07-17 18:14:40 +0800

    1.7

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -17,6 +17,7 @@ class wechatCallbackapiTest
         if($this->checkSignature()){
             header('content-type:text');
         	echo $echoStr;
+            $this->responseMsg();
         	exit;
         }
     }
