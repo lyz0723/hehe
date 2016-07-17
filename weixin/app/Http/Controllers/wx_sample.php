d5228ca0b279522ef542c70d6515bf2c83542d03
lyz <360195045@qq.com>
2016-07-17 20:54:21 +0800

    3.0

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -5,7 +5,7 @@
 //define your token
 define("TOKEN", "$token");
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+//$wechatObj->valid();
 //开启自动回复功能
 echo $wechatObj->responseMsg();
 class wechatCallbackapiTest
