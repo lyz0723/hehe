977bf600d01368514b55c250779b672e05dd6f22
lyz <360195045@qq.com>
2016-07-17 14:31:15 +0800

    1.2

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -40,24 +40,24 @@ class wechatCallbackapiTest
             $toUsername = $postObj->ToUserName;
             $keyword = trim($postObj->Content);
             $time = time();
-            $textTpl = "<xml>
+            if($postObj->MsgType=='text'){
+                $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
 							<MsgType><![CDATA[%s]]></MsgType>
 							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
 							</xml>";
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }else{
-                echo "Input something...";
+                if(!empty( $keyword ))
+                {
+                    $msgType = "text";
+                    $contentStr = "Welcome to wechat world!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;die;
+                }else{
+                    echo "Input something...";
+                }
             }
-
         }else {
             echo "";
             exit;
