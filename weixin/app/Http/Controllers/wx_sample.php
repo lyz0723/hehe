03d6330539e06235f9c71f0eddeb992963b05a6e
lyz <360195045@qq.com>
2016-07-17 10:56:55 +0800

    tuling

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -32,9 +32,13 @@ class wechatCallbackapiTest
                    the best way is to check the validity of xml by yourself */
                 libxml_disable_entity_loader(true);
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                //发送方账号(一个openid)
                 $fromUsername = $postObj->FromUserName;
+                //开发者微信号
                 $toUsername = $postObj->ToUserName;
                 $keyword = trim($postObj->Content);
+//                //获取用户发送消息的类型
+//                $msgType=$postObj->MsgType;
                 $time = time();
                 $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
@@ -43,21 +47,30 @@ class wechatCallbackapiTest
 							<MsgType><![CDATA[%s]]></MsgType>
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
+							</xml>";
+				if(!empty( $keyword )) {
+                    $msgType = "text";
+                    $contentStr = "Welcome to wechat world!";
+                    $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                    echo $resultStr;
                 }else{
-                	echo "Input something...";
+                    //定义回复类型
+                    $msgType="text";
+                    //定义URL链接操作
+                    $url="http://www.tuling123.com/openapi/api?key=1f3a6c1438f6935ea3344fc678cc509c&info={$keyword}";
+                    $str=file_get_contents($url);
+                    $json=json_decode($str);
+                    //定义回复内容
+                    $contentStr=$json->text;
+                    //格式化字符串
+                    $result=sprintf($textTpl,$fromUsername,$toUsername, $contentStr, $time);
+                    //返回数据给客户端
+                    echo $result;
                 }
 
-        }else {
-        	echo "";
-        	exit;
-        }
+                }else{
+                    echo "Input something...";
+                }
     }
 		
 	private function checkSignature()
