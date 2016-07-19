7434153e207c954f196041d2072c9b96ec1ce3d2
lyz <360195045@qq.com>
2016-07-19 14:46:36 +0800

    ss

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -44,6 +44,7 @@ class wechatCallbackapiTest
                 libxml_disable_entity_loader(true);
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
+            echo $fromUsername;
                 $toUsername = $postObj->ToUserName;
                 $keyword = trim($postObj->Content);
                 $time = time();
