cade08456d6053e9f04014d14d7e77fdf143cdd8
lyz <360195045@qq.com>
2016-07-19 14:25:06 +0800

    ss

--- weixin/app/Http/Controllers/wx_sample.php
+++ weixin/app/Http/Controllers/wx_sample.php
@@ -13,6 +13,7 @@ class wechatCallbackapiTest
 
         //valid signature , option
         if($this->checkSignature()){
+            header('content-type:text');
         	echo $echoStr;
         	exit;
         }
