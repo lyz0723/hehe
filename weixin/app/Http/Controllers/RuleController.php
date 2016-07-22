5466f0a1333c0038e856ff76e2d3922319b6b8d9
lyz <360195045@qq.com>
2016-07-22 08:35:12 +0800

    1.3

--- weixin/app/Http/Controllers/RuleController.php
+++ weixin/app/Http/Controllers/RuleController.php
@@ -31,16 +31,16 @@ class RuleController extends Controller{
         $title=Request::input('tit');
         //接受图片
         $image=Input::file('pic');
-        $allowed = ["png", "jpg", "gif","jpeg"];
-        if ($image->getClientOriginalExtension() && !in_array( $image->getClientOriginalExtension(), $allowed)) {
-            return ['error' => '您只能上传 png, jpg 或 gif格式的图片'];
-        }
-        $path = '../public/uploads/';
-        $extension = $image->getClientOriginalExtension();
-        //echo $extension;die;
-        $filename = str_random(10).'.'.$extension;
-        //echo $filename;die;
-        $image->move($path , $filename);  // 移动文件到指定目录
+//        $allowed = ["png", "jpg", "gif","jpeg"];
+//        if ($image->getClientOriginalExtension() && !in_array( $image->getClientOriginalExtension(), $allowed)) {
+//            return ['error' => '您只能上传 png, jpg 或 gif格式的图片'];
+//        }
+//        $path = '../public/uploads/';
+//        $extension = $image->getClientOriginalExtension();
+//        //echo $extension;die;
+//        $filename = str_random(10).'.'.$extension;
+//        //echo $filename;die;
+//        $image->move($path , $filename);  // 移动文件到指定目录
 
         $i_content=Request::input('nei');
         $url=Request::input('lian');
@@ -49,7 +49,7 @@ class RuleController extends Controller{
 //            $uid=Session::get('uid');
         //实例化对象
         $arr=new We_rule();
-        $arr->add($name,$type,$content,$key,$title,$filename,$i_content,$url);
+        $arr->add($name,$type,$content,$key,$title,$image,$i_content,$url);
         return redirect('rule');
     }
 
