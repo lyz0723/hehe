f2ff61785d4531c7235acbd9a7ec6c853199a6c4
lyz <360195045@qq.com>
2016-07-24 12:52:45 +0800

    2.4

--- weixin/app/Http/Controllers/MenuController.php
+++ weixin/app/Http/Controllers/MenuController.php
@@ -25,16 +25,12 @@ class MenuController extends Controller{
         $file=file_get_contents($url);
         $data=json_decode($file,true);
         $Accesstoken=$data['access_token'];
-        echo $Accesstoken;die;
-        $url="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$Accesstoken;
-        $data=$arr['aa'];
-        $this->weixinPost($url,"POST",$data);
-    }
 
-    //微信curl提交
-    public function weixinPost($url,$method,$data){
+        $url1="https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$Accesstoken;
+        $data=$arr['aa'];
+        $method="POST";
         $ch = curl_init();   //1.初始化
-        curl_setopt($ch, CURLOPT_URL, $url); //2.请求地址
+        curl_setopt($ch, CURLOPT_URL, $url1); //2.请求地址
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);//3.请求方式
         //4.参数如下
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);//https
@@ -54,6 +50,34 @@ class MenuController extends Controller{
             return curl_error($ch);
         }
         return $tmpInfo;
-        curl_close($ch);//8.关闭
+
+
+        //$this->weixinPost($url,"POST",$data);
     }
+
+    //微信curl提交
+//    public function weixinPost($url,$method,$data){
+//        $ch = curl_init();   //1.初始化
+//        curl_setopt($ch, CURLOPT_URL, $url); //2.请求地址
+//        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);//3.请求方式
+//        //4.参数如下
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);//https
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+//        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');//模拟浏览器
+//        curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+//        curl_setopt($ch, CURLOPT_HTTPHEADER,array('Accept-Encoding: gzip, deflate'));//gzip解压内容
+//        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+//
+//        if($method=="POST"){//5.post方式的时候添加数据
+//            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//        }
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//        $tmpInfo = curl_exec($ch);//6.执行
+//
+//        if (curl_errno($ch)) {//7.如果出错
+//            return curl_error($ch);
+//        }
+//        return $tmpInfo;
+//        curl_close($ch);//8.关闭
+//    }
 }
