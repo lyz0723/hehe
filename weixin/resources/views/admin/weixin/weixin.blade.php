4372cf1830e3c4755262eb0fe3a7b60d25755ca4
lyz <360195045@qq.com>
2016-07-26 20:40:19 +0800

    1.1

--- weixin/resources/views/admin/weixin/weixin.blade.php
+++ weixin/resources/views/admin/weixin/weixin.blade.php
@@ -5,6 +5,11 @@
     <title></title>
 </head>
 <body>
+<li class="label_item wxapi_index_item"><a class="label_inner" href="#menu-scan">微信扫一扫接口</a></li>
+<h3 id="menu-scan">微信扫一扫</h3>
+<span class="desc">调起微信扫一扫接口</span>
+<button class="btn btn_primary" id="scanQRCode0">scanQRCode(微信处理结果)</button>
+<button class="btn btn_primary" id="scanQRCode1">scanQRCode(直接返回结果)</button>
 </body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
@@ -32,19 +37,24 @@
     });
     wx.ready(function () {
         // 在这里调用 API
-        wx.scanQRCode({
+        document.querySelector('#scanQRCode1').onclick = function(){
+            wx.scanQRCode({
 
-            needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
 
-            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
 
-            success: function (res) {
+                success: function (res) {
 
-                var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
 
-            }
+                }
 
-        });
+            });
+        } ;
+
+
+
     });
 </script>
 </html>
