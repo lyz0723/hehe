2fc7cc09bb92cef87293344c9f11df0efb1f19f5
lyz <360195045@qq.com>
2016-07-26 18:12:08 +0800

    1.3

--- weixin/app/Http/Controllers/WeixinController.php
+++ weixin/app/Http/Controllers/WeixinController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 
 class WeixinController extends Controller
 {
-    public function index(){
-        echo $this->wx_get_token();
-    }
+    //获取Access_token值
     function wx_get_token() {
-
-
             $res = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx9036c924e93284c6&secret=b6ace35d7f3820f253b6c770d6a028e4');
             $res = json_decode($res, true);
             $token = $res['access_token'];
-            
-
-        return $token;
+            return $token;
+    }
+    //获取jsapi_ticket
+    function jsapi_ticket(){
+        $Access_token=$this->wx_get_token();
+       $url="https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$Access_token&type=jsapi";
+        $file=file_get_contents($url);
+        $data=json_decode($file,true);
+        $ticket=$data['ticket'];
+        echo $ticket;
     }
 }
